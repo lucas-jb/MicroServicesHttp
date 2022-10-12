ae37ff4a35c5605033c1db48e223f6dde44651ab
Lucas Juárez Barrios <lucasjuarezbarrios@gmail.com>
2022-10-12 23:04:26 +0200

    refactor

--- MicroServiceBusqueda/Program.cs
+++ MicroServiceBusqueda/Program.cs
@@ -1,11 +1,33 @@
-var builder = WebApplication.CreateBuilder(args);
+using MicroServiceBusqueda.Interfaces;
+using MicroServiceBusqueda.Services;
 
+var builder = WebApplication.CreateBuilder(args);
+ConfigurationManager configuration = builder.Configuration;
+IWebHostEnvironment environment = builder.Environment;
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+
+builder.Services.AddSingleton<IClientesServices, ClienteServices>();
+builder.Services.AddSingleton<IProductosServices, ProductoServices>();
+builder.Services.AddSingleton<IVentasServices, VentaServices>();
+
+builder.Services.AddHttpClient("clienteService", c =>
+{
+    c.BaseAddress = new Uri(configuration["Services:Clientes"]);
+});
+builder.Services.AddHttpClient("productoService", c =>
+{
+    c.BaseAddress = new Uri(configuration["Services:Productos"]);
+});
+builder.Services.AddHttpClient("ventaService", c =>
+{
+    c.BaseAddress = new Uri(configuration["Services:Ventas"]);
+});
 
 var app = builder.Build();
 
