0588fddde8f69b63ad76d9c389aa12be06d78842
Lucas Juárez Barrios <lucasjuarezbarrios@gmail.com>
2022-10-12 20:02:22 +0200

    fix var name

--- MicroServiceClientes/DAL/ClientesProvider.cs
+++ MicroServiceClientes/DAL/ClientesProvider.cs
@@ -4,22 +4,22 @@ namespace MicroServiceClientes.DAL
 {
     public class ClientesProvider : IClientesProvider
     {
-        private readonly List<Cliente> repo = new List<Cliente>();
+        private readonly List<Cliente> ClientesRepository = new List<Cliente>();
 
         public ClientesProvider()
         {
-            repo.Add(new Cliente() { Id = "1", Name = "Lucas", City = "Logroño" });
-            repo.Add(new Cliente() { Id = "2", Name = "Juan", City = "Madrid" });
-            repo.Add(new Cliente() { Id = "3", Name = "Jacinto", City = "Zaragoza" });
-            repo.Add(new Cliente() { Id = "4", Name = "Daniel", City = "Barcelona" });
-            repo.Add(new Cliente() { Id = "5", Name = "Sergio", City = "Valencia" });
-            repo.Add(new Cliente() { Id = "6", Name = "David", City = "Burgos" });
-            repo.Add(new Cliente() { Id = "7", Name = "Pablo", City = "Vigo" });
-            repo.Add(new Cliente() { Id = "8", Name = "Pedro", City = "Sevilla" });
+            ClientesRepository.Add(new Cliente() { Id = "1", Name = "Lucas", City = "Logroño" });
+            ClientesRepository.Add(new Cliente() { Id = "2", Name = "Juan", City = "Madrid" });
+            ClientesRepository.Add(new Cliente() { Id = "3", Name = "Jacinto", City = "Zaragoza" });
+            ClientesRepository.Add(new Cliente() { Id = "4", Name = "Daniel", City = "Barcelona" });
+            ClientesRepository.Add(new Cliente() { Id = "5", Name = "Sergio", City = "Valencia" });
+            ClientesRepository.Add(new Cliente() { Id = "6", Name = "David", City = "Burgos" });
+            ClientesRepository.Add(new Cliente() { Id = "7", Name = "Pablo", City = "Vigo" });
+            ClientesRepository.Add(new Cliente() { Id = "8", Name = "Pedro", City = "Sevilla" });
         }
         public Task<Cliente> GetAsync(string id)
         {
-            var cliente = repo.FirstOrDefault(c => c.Id == id);
+            var cliente = ClientesRepository.FirstOrDefault(c => c.Id == id);
             return Task.FromResult(cliente);
         }
     }
